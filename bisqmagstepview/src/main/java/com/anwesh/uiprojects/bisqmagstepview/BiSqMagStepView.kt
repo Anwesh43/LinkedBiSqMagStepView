57dcb31a6e3be08b9d9712fcac9f4c89fe9f83cf
Anwesh43 <anweshthecool0@gmail.com>
2018-11-15 10:01:51 +0530

    importing libraries and defining nodes

--- bisqmagstepview/src/main/java/com/anwesh/uiprojects/bisqmagstepview/BiSqMagStepView.kt
+++ bisqmagstepview/src/main/java/com/anwesh/uiprojects/bisqmagstepview/BiSqMagStepView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.bisqmagstepview
 /**
  * Created by anweshmishra on 15/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 4
+val parts : Int = 2
+val scGap : Float = 0.1f / parts
+val scDiv : Double = 1.0 / parts
+val SIZE_FACTOR : Int = 3
+val STRKE_FACTOR : Int = 80
+val FORE_GROUND_COLOR : Int = Color.parseColor("#283593")
