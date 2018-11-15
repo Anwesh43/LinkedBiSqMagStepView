3fc40cdc32cedba754ba518baf833deec0242e44
Anwesh43 <anweshthecool0@gmail.com>
2018-11-15 11:18:28 +0530

    adding BiSqMagStepView create in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbisqmagstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbisqmagstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbisqmagstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bisqmagstepview.BiSqMagStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiSqMagStepView.create(this)
     }
 }
