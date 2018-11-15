a047311938905665648a39379448d76507d61fa9
Anwesh43 <anweshthecool0@gmail.com>
2018-11-15 11:03:25 +0530

    adding BSMSNode class

--- bisqmagstepview/src/main/java/com/anwesh/uiprojects/bisqmagstepview/BiSqMagStepView.kt
+++ bisqmagstepview/src/main/java/com/anwesh/uiprojects/bisqmagstepview/BiSqMagStepView.kt
@@ -128,4 +128,49 @@ class BiSqMagStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BSMSNode(var i : Int, val state : State = State()) {
+
+        private var next : BSMSNode? = null
+
+        private var prev : BSMSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BSMSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBSMSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BSMSNode {
+            var curr : BSMSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
